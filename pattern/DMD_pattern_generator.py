1e995d94b1418a9513da718c9f2e7f28bad9e4be
zmjm <zmjm@uchicago.edu>
2023-07-19 19:37:14 -0500

    add pattern generation function

--- pattern/DMD_pattern_generator.py
+++ pattern/DMD_pattern_generator.py
@@ -6,6 +6,8 @@ from tkinter.filedialog import askopenfilename
 import math
 import os
 
+from DMD_pattern_function import *
+
 DMD_ROWS = 1140
 DMD_COLS = 912
 
@@ -13,33 +15,47 @@ REAL_ROWS = math.ceil((DMD_ROWS-1) / 2) + DMD_COLS
 REAL_COLS = DMD_COLS + (DMD_ROWS-1) // 2
 
 # Convert DMD space (row, col) to real space
-def realSpaceCoor(row, col):
-    return (math.ceil(row / 2) + col, DMD_COLS - 1 + row//2 - col)
+def realSpaceRow(row, col):
+    return math.ceil(row / 2) + col
 
-def generateTemplate():
+def realSpaceCol(row, col):
+    return DMD_COLS - 1 + row//2 - col
+
+def generateTemplate(color=1):
     template = Image.new("RGB", size=(REAL_COLS, REAL_ROWS), color='#ff0000')
 
     for row in range(DMD_ROWS):
         for col in range(DMD_COLS):
-            real_row, real_col = realSpaceCoor(row, col)
-            template.putpixel((real_col, real_row), value=(255, 255, 255))
+            real_row, real_col = realSpaceRow(row, col), realSpaceCol(row, col)
+            if color == 1:
+                template.putpixel((real_col, real_row), value=(255, 255, 255))
+            elif color == 0:
+                template.putpixel((real_col, real_row), value=(0, 0, 0))
 
     draw = ImageDraw.Draw(template)
     font = ImageFont.truetype("arial.ttf", 30)
 
     # Add labels on the corners
-    corner00 = realSpaceCoor(0, 0)[1] - 100, realSpaceCoor(0, 0)[0]
-    corner10 = realSpaceCoor(DMD_ROWS-1, 0)[1] - 150, realSpaceCoor(DMD_ROWS-1, 0)[0] + 150
-    corner11 = realSpaceCoor(DMD_ROWS-1, DMD_COLS-1)[1] + 50, realSpaceCoor(DMD_ROWS-1, DMD_COLS-1)[0] - 50
+    corner00 = realSpaceCol(0, 0) - 100, realSpaceRow(0, 0)
+    corner10 = realSpaceCol(DMD_ROWS-1, 0) - 150, realSpaceRow(DMD_ROWS-1, 0) + 150
+    corner11 = realSpaceCol(DMD_ROWS-1, DMD_COLS-1) + 50, realSpaceRow(DMD_ROWS-1, DMD_COLS-1) - 50
 
     draw.text(corner00, '(0, 0)', font=font, fill=0)
     draw.text(corner10, f'({DMD_ROWS-1}, 0)', font=font, fill=0)
     draw.text(corner11, f'({DMD_ROWS-1}, {DMD_COLS-1})', font=font, fill=0)
 
     template.save(f'DMD_template_{DMD_ROWS}x{DMD_COLS}.bmp')
+    return template.convert("1")
 
 def generatePattern(row=DMD_ROWS//2, col=DMD_COLS//2):
-    pass
+    real_row, real_col = realSpaceRow(row, col), realSpaceCol(row, col)
+    real_image = generateTemplate(color=0)
+    
+    # for radius in [10, 20, 50, 100, 200]:
+    #     real_image = circle_bright(real_image, real_col, real_row, radius=radius)
+    #     dmd_image = convertImageToDMDArray(real_image)
+    #     dmd_image.show()
+    #     dmd_image.save(f'DMD_pattern_circle_bright_{2*radius}.bmp')
 
 def convertImageToDMDArray(image):
     pixels = image.load()
@@ -51,25 +67,26 @@ def convertImageToDMDArray(image):
     #the corresponding pixel value from the real space image
     for row in range(DMD_ROWS):
         for col in range(DMD_COLS):
-            real_row, real_col = realSpaceCoor(row, col)
+            real_row, real_col = realSpaceRow(row, col), realSpaceCol(row, col)
             dmd_image.putpixel((col, row), value=pixels[real_col, real_row])
     return dmd_image
 
 if __name__ == '__main__':
     # generateTemplate()
+    generatePattern()
 
-    root = Tk()
-    root.withdraw()
-    file_path = askopenfilename(title='Select Modified BMP Image Template', filetypes=[('BMP Files', '*.bmp')])
-    directory, filename = os.path.split(file_path)
-    filename_new = 'DMD_pattern_' + os.path.splitext(filename)[0] + '.bmp'
+    # root = Tk()
+    # root.withdraw()
+    # file_path = askopenfilename(title='Select Modified BMP Image Template', filetypes=[('BMP Files', '*.bmp')])
+    # directory, filename = os.path.split(file_path)
+    # filename_new = 'DMD_pattern_' + os.path.splitext(filename)[0] + '.bmp'
 
-    image = Image.open(file_path)
-    image = image.convert('1')
+    # image = Image.open(file_path)
+    # image = image.convert('1')
 
-    #Convert the template to a DMD Image
-    dmd_image = convertImageToDMDArray(image)
+    # #Convert the template to a DMD Image
+    # dmd_image = convertImageToDMDArray(image)
 
-    #Show the converted DMD pattern and save it to your directory
-    dmd_image.show()
-    dmd_image.save(os.path.join(directory, filename_new))
+    # #Show the converted DMD pattern and save it to your directory
+    # dmd_image.show()
+    # dmd_image.save(os.path.join(directory, filename_new))
