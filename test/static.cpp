8663eac572e698381d9df30c1cfa102ec43b4fec
zmjm <zmjm@uchicago.edu>
2023-10-03 00:14:50 -0500

    add source code to display static pattern loaded from BMP

--- test/static.cpp
+++ test/static.cpp
@@ -1,83 +1,52 @@
 #include <SDL3/SDL.h>
 #include <windows.h>
+#include <commdlg.h>
+#include <iostream>
 #include <vector>
 #include <string>
-#include <iostream>
+#include <filesystem>
 
 const int LINE_STEP = 100;
 const int FRAME_SAMPLE = 60;
 
+// Set the initial directory (change this to your desired path)
+const std::string INITIAL_DIR = std::filesystem::absolute("../resources").string();
+
 SDL_Window* window = NULL;
 SDL_Surface* surface = NULL;
 SDL_Renderer* renderer = NULL;
-SDL_Texture* texture = NULL;
-Uint32* data = NULL;
-SDL_DisplayMode mode;
-SDL_Rect rect;
 
-std::vector<std::wstring> openWindow() {
-    OPENFILENAME ofn;
-    TCHAR szFile[260];
+void openBmpFileDialog(std::vector<std::string>& selectedFiles)
+{
+    OPENFILENAMEA ofn; // Use ANSI version
 
-    ZeroMemory(&ofn, sizeof(OPENFILENAME));
-    ofn.lStructSize = sizeof(OPENFILENAME);
-    ofn.hwndOwner = NULL;  // If you have a parent window, set it here.
+    char szFile[MAX_PATH] = "";
+
+    ZeroMemory(&ofn, sizeof(ofn));
+    ofn.lStructSize = sizeof(ofn);
     ofn.lpstrFile = szFile;
     ofn.lpstrFile[0] = '\0';
     ofn.nMaxFile = sizeof(szFile);
-    ofn.lpstrFilter = TEXT("Bitmap Files (*.bmp)\0*.bmp\0All Files (*.*)\0*.*\0");
+    ofn.lpstrFilter = "Bitmap Images (*.bmp)\0*.bmp\0All Files (*.*)\0*.*\0";
     ofn.nFilterIndex = 1;
-    ofn.lpstrFileTitle = NULL;
-    ofn.nMaxFileTitle = 0;
-    ofn.lpstrInitialDir = NULL;
+    ofn.lpstrTitle = "Select .bmp Files";
     ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_ALLOWMULTISELECT;
+    ofn.lpstrInitialDir = INITIAL_DIR.c_str(); // Set initial directory
 
-    if (GetOpenFileName(&ofn) == TRUE) {
-        // The selected file path(s) are now stored in szFile.
-        // Parse the paths and store them in a vector.
-        std::vector<std::wstring> selectedFiles;
-        TCHAR* pFile = szFile;
-        while (*pFile) {
-            int len = lstrlen(pFile);
-            std::wstring filePath;
-            if (len > 0) {
-                int wideCharLen = MultiByteToWideChar(CP_ACP, 0, pFile, len, NULL, 0);
-                if (wideCharLen > 0) {
-                    std::vector<wchar_t> wideFilePath(wideCharLen + 1);
-                    MultiByteToWideChar(CP_ACP, 0, pFile, len, &wideFilePath[0], wideCharLen);
-                    wideFilePath[wideCharLen] = L'\0';
-                    filePath = &wideFilePath[0];
-                }
+    if (GetOpenFileNameA(&ofn) == TRUE) // Use ANSI version
+    {
+        char* p = szFile;
+        selectedFiles.push_back(p);
+
+        while (*p != '\0')
+        {
+            p += strlen(p) + 1;
+            if (*p != '\0')
+            {
+                selectedFiles.push_back(p);
             }
-            selectedFiles.push_back(filePath);
-            pFile += len + 1;
         }
-
-        return selectedFiles;
-    } else {
-        // If the user canceled the selection, return an empty vector.
-        return std::vector<std::wstring>();
     }
-}
-
-int main() {
-    std::vector<std::wstring> selectedFiles = openWindow();
-
-    if (!selectedFiles.empty()) {
-        // Process the selected files here.
-        for (const std::wstring& filePath : selectedFiles) {
-            MessageBox(NULL, filePath.c_str(), TEXT("Selected File"), MB_OK);
-        }
-    } else {
-        MessageBox(NULL, TEXT("File selection canceled."), TEXT("Info"), MB_OK);
-    }
-
-    return 0;
-}
-
-void renderTexture(){
-    SDL_UpdateTexture(texture, NULL, data, surface->pitch);
-    SDL_RenderTexture(renderer, texture, NULL, NULL);
 }
 
 int initWindow(){
@@ -95,15 +64,17 @@ int initWindow(){
     else std::cout << "Number of displays connected: " << count << std::endl;
 
     //Get display mode
-    SDL_GetDesktopDisplayMode(1, & mode);
-    std::cout << "Display mode of #" << count << " Display:\n\tw = " << mode.w
+    SDL_DisplayMode mode;
+    SDL_GetDesktopDisplayMode(count - 1, & mode);
+    std::cout << "Display mode of #" << count - 1 << " Display:\n\tw = " << mode.w
                 << "\n\th = " << mode.h << "\n\trefreshrate = "
                 << mode.refresh_rate << "\n\tpixel format = " 
                 << SDL_GetPixelFormatName(mode.format) <<std::endl;
 
     //Get monitor bounds
-    SDL_GetDisplayBounds(1, & rect);
-    std::cout << "Display bound of #" << count << " Display:\n\tx = " << rect.x << "\n\ty = " << rect.y 
+    SDL_Rect rect;
+    SDL_GetDisplayBounds(count - 1, & rect);
+    std::cout << "Display bound of #" << count - 1 << " Display:\n\tx = " << rect.x << "\n\ty = " << rect.y 
                 << "\n\tw = " << rect.w << "\n\th = " << rect.h << std::endl;
 
     //Create a full screen window
@@ -130,48 +101,74 @@ int initWindow(){
         return -1;
     }
 
-    //Create a texture for rendering context
-    texture = SDL_CreateTexture(renderer, surface->format->format, SDL_TEXTUREACCESS_STREAMING, rect.w, rect.h);
-    data = (Uint32*) calloc(rect.w * rect.h, unsigned(surface->format->BytesPerPixel));
-
     return 0;
 }
 
-int quitWindow(){
-    SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit(); 
-    return 0;
-}
+int main()
+{
+    std::vector<std::string> selectedFiles;
 
-// int main(int argc, char* args[]){
+    openBmpFileDialog(selectedFiles);
     
-//     std::vector<std::wstring> selectedFiles = openWindow();
+    initWindow();
 
-//     if (!selectedFiles.empty()) {
-//         // Process the selected files here.
-//         for (const std::wstring& filePath : selectedFiles) {
-//             MessageBox(NULL, filePath.c_str(), TEXT("Selected File"), MB_OK);
-//         }
-//     } else {
-//         MessageBox(NULL, TEXT("File selection canceled."), TEXT("Info"), MB_OK);
-//     }
+    if (!selectedFiles.empty())
+    {
+        std::cout << "Selected files:" << std::endl;
+        for (const std::string& file : selectedFiles)
+        {
+            std::cout << file << std::endl;
+            
+            SDL_Surface* image = SDL_LoadBMP(file.c_str());
+            if (!image){
+                std::cout << "Unable to load image! SDL_Error: " << SDL_GetError() << std::endl;
+                return -1;
+            }
 
-//     initWindow();
+            // Create a texture from the loaded image
+            SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, image);
+            SDL_DestroySurface(image);
+            if (!texture) {
+                std::cerr << "Texture creation failed: " << SDL_GetError() << std::endl;
+                return 1;
+            }
 
-//     SDL_Event e;
-//     bool quit = false;
-//     int iter = 0;
-//     while (!quit){  
-//         iter++;
-//         SDL_PollEvent( &e );
-//         if( e.type == SDL_QUIT ) 
-//             quit = true;
+            bool quit = false;
+            SDL_Event event;
 
-//         //Update the surface
-//         SDL_RenderPresent(renderer);
-//     }
-// 	quitWindow();
+            while (!quit) {
+                // Clear the renderer
+                SDL_RenderClear(renderer);
 
-// 	return 0;
-// }
+                // Copy the texture to the renderer
+                SDL_RenderTexture(renderer, texture, NULL, NULL);
+
+                // Present the renderer
+                SDL_RenderPresent(renderer);
+
+                while (SDL_PollEvent(&event) != 0) {
+                    if (event.type == SDL_QUIT) {
+                        quit = true;
+                    } else if (event.type == SDL_KEYDOWN) {
+                        if (event.key.keysym.sym == SDLK_ESCAPE) {
+                            quit = true;
+                        }
+                    }
+                }
+                
+            }
+
+        }
+        // Clean up and quit
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit(); 
+
+    }
+    else
+    {
+        std::cout << "No files selected." << std::endl;
+    }
+
+    return 0;
+}
